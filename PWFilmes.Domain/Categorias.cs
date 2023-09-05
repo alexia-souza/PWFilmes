9e5524ba51cf0a960c8eb4fbf0de4fd259555051
alexia-souza <alexia.souza13@etec.sp.gov.br>
2023-09-04 22:43:45 -0300

    Aula 04-09-2023

---
+++ PWFilmes.Domain/Categorias.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PWFilmes.Domain
+{
+    public class Categorias
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Cor { get; set; }
+
+
+    }
+}
