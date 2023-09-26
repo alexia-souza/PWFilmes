421b6966c7e255ed1c2503a8e226bbf968f141c6
Aluno <aluno@etec.sp.gov.br>
2023-09-25 22:47:05 -0300

    Atividades Aula 25/09/2023

---
+++ PWFilmes.API/Context/PWFilmesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PWFilmes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWFilmes.API.Context
+{
+    public class PWFilmesContext: DbContext
+    {
+        public DbSet<Categoria> CategoriaSet {get; set;}
+             
+    }
+}
